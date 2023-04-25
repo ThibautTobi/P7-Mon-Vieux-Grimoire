59036cb67b3810c4abd0980e1010e91666f36cd1
thibaut denis <thibaut_du_denis@hotmail.fr>
2023-04-25 17:29:58 +0200

    initialisation Back-end

---
+++ Back-end/server.js
@@ -0,0 +1,55 @@
+const http = require('http');
+const app = require('./app');
+
+/**********************************    gestion des port cours open classrooms
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+*/
+
+app.set('port', process.env.PORT || 3000); // delete
+const server = http.createServer(app);
+
+/***************************************************** deuxiéme partie 
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
+*/
+
+// j'ecoute le port 3000 mais si il est utiliser dans certains cas variable d'environement (si la plateforme propose un port par defaut ce sera celui la qui sera ecouter)
+server.listen(process.env.PORT || 3000);// delete
